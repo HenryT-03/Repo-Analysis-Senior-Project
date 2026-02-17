42161c50b5e9f45ff7fbf0d556e1c9ba6f4c93d3
Jacob Callicott <jacobc54642@gmail.com>
2026-02-16 19:28:59 -0600

    Used backend in frontend

--- dashboard-backend/app.py
+++ dashboard-backend/app.py
@@ -1,7 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/api/hello")
+@app.route("/api/hello", methods=["GET"])
 def hello():
-    return jsonify(message="backend api calll")
+    return jsonify(message="Hello from Flask!")
+
+@app.route("/api/add", methods=["POST"])
+def add():
+    data = request.json
+    return jsonify(result=data["a"] + data["b"])
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- dashboard-frontend/src/main.ts
+++ dashboard-frontend/src/main.ts
@@ -20,5 +20,18 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </p>
   </div>
 `
+fetch("http://localhost:5000/api/hello")
+  .then(res => res.json())
+  .then(data => console.log(data.message));
+  
+fetch("http://localhost:5000/api/add", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({ a: 2, b: 3 })
+})
+  .then(res => res.json())
+  .then(data => console.log(data.result));
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
