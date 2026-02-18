427e0b861d21299ef9d7dd3a228387d4741acb5f
testsiter <easylifeanimations@gmail.com>
2026-02-17 21:05:57 -0600

    Changing app to link to dashboard
    this is temporary for the UI so that the UI can be seen, should link back to the backend at some point in the future

--- dashboard-frontend/dashboard-frontend/src/App.tsx
+++ dashboard-frontend/dashboard-frontend/src/App.tsx
@@ -1,34 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from "./Elements/GroupviewNavbar";
-import SquareGrid from "./Elements/GroupviewSquareGrid";
+import React from 'react';
+import Dashboard from './Dashboard.tsx';
 
 const App: React.FC = () => {
-  const [squareCount, setSquareCount] = useState(9);
-
-  return (
-    <>
-      <Navbar
-        onAdd={() => setSquareCount((prev) => prev + 1)}
-        onReset={() => setSquareCount(0)}
-      />
-      <SquareGrid count={squareCount} />
-    </>
-  );
+  return <Dashboard />;
 };
 
-fetch("http://localhost:5000/api/hello")
-  .then(res => res.json())
-  .then(data => console.log(data.message));
-  
-fetch("http://localhost:5000/api/add", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({ a: 2, b: 3 })
-})
-  .then(res => res.json())
-  .then(data => console.log(data.result));
-
-export default App
+export default App;
